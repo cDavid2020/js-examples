5165f9401c8cd4583e8e9e6187c9d51acc45b449
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2022-12-18 10:16:59 -0600

    push

---
+++ Variables.js
@@ -0,0 +1,27 @@
+// Variables
+// a variable is a containter for storing data
+// a variable behaves as if it was the value that it contains
+
+/* two steps
+1. declaration (var, let, const)
+2. assignment  (=) */
+
+let firstName = "John"; //strings (letters only)
+let age = 21; //numbers (numbers only)
+let student = true; //boolean (true or false)
+
+age = age +1;
+
+console.log(firstName);
+console.log("hello")
+console.log("Hello", firstName);
+console.log("You are", age, "years old");
+
+console.log("Enrolled", student);
+
+document.getElementById("p1").innerHTML = 
+  
+//* to edit the html dom from within js like this */
+document.getElementById("p1").innterHTML = "Hello " + firstName;
+document.getElementById("p2").innterHTML = "You are " age + " years old";
+document.getElementById("p3").innterHTML = "Enrolled: " + student;
---
+++ call-back-function.js
@@ -0,0 +1,44 @@
+//callback - a function passed as an argument to another function. callbacks ensure that a function is not going to run before a task is completed (when one function has to wait for another function) which helps to avoid errors, similar to a pause. callbacks are asynchronous code because they are not executed immediately.
+
+//callback example 
+
+let total = sum(2,3);
+
+functionion sum(){
+  let result = x+y; 
+  return result
+}
+
+function displayConsole(output){
+   console.log("output");
+}
+
+//function to display output to the DOM
+function displayDom(output){
+    document.getElementById("myLabel").innerHTML = output;
+  
+}
+
+//another way to use 👆is w a callback function
+sum(2,3,displayConsole);
+
+function sum(x,y,myFunction){ //renamed callBack to myFunction. you can rename parameters
+   let result = x + y;
+}
+
+function displayConsole(output){
+   console.log("output");
+}
+
+//function to display output to the DOM
+function displayDom(output){
+    document.getElementById("myLabel").innerHTML = output;
+
+
+
+//callback example
+cacheconsole.log("1");
+setTimeout() => {
+    console.log("2");
+}, 1000);
+console.log("3");
