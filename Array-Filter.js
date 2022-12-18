7ce7d0d6cb0c264460461cf81a1acb25717a46cf
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2022-12-18 13:34:33 -0600

    add array.reduce

--- Array-Filter.js
+++ Array-Filter.js
@@ -3,6 +3,9 @@
 //array.filter() = creates a new array with all elements that pass the test provided by a function
 
 let ages = [32, 33, 16, 19, 42, 85, 40];
+let adults = ages.filter(checkAge);
+
+adults.forEach(print);
 
 // a function to to check age
 function checkAge(element) {
